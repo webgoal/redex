f6d669f3f51ee1759de599bfa0ccc14bed93ec9d
Gustavo Canedo <gknedo@gmail.com>
2017-07-24 20:39:31 -0300

    adiciona config para url

--- lib/redex/request/transaction_request.rb
+++ lib/redex/request/transaction_request.rb
@@ -1,17 +1,16 @@
 module Redex
 	module Request
 		class TransactionRequest < BaseRequest
-			attr_accessor :order_id, :amount, :installments, :recorrence, :invoice_note,
+			attr_accessor :order_id, :amount, :installments, :invoice_note,
 										:card_number, :card_cvc, :card_holder_name,
 										:card_expiration_month, :card_expiration_year,
 										:auto_capture
-			attr_reader :subscription, :origin
+			attr_reader :recorrence, :origin
 
 	    def initialize(params = {})
 				@order_id = params[:order_id]
 				@amount = params[:amount]
-				@installments = params[:installments]
-				@recorrence = params[:recorrence]
+				@installments = params.fetch(:installments, 1)
 				@invoice_note = params[:invoice_note]
 				@card_number = params[:card_number]
 				@card_cvc = params[:card_cvc]
@@ -19,7 +18,7 @@ module Redex
 				@card_expiration_month = params[:card_expiration_month]
 				@card_expiration_year = params[:card_expiration_year]
 				@auto_capture = params[:auto_capture]
-	      @subscription = false
+	      @recorrence = false
 	      @origin = 1
 	    end
 
