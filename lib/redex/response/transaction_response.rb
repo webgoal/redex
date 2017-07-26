9abc7dc3baf029a2c10338209feec559c7104319
Diego Thomaz <diego@webgoal.com.br>
2017-07-26 11:01:29 -0300

    Adicionado parser para sequential_id & transaction_id

--- lib/redex/response/transaction_response.rb
+++ lib/redex/response/transaction_response.rb
@@ -1,7 +1,7 @@
 module Redex
 	module Response
 		class TransactionResponse < BaseResponse
-			attr_reader :code, :message, :order_id, :created_at
+			attr_reader :code, :message, :order_id, :created_at, :credit_card_authorization_id, :sequential_id, :transaction_id
 			def initialize(httpi_response)
 				cleaned_response = BaseResponse.clear(httpi_response)
 				@code = cleaned_response[:CodRet].to_i
@@ -9,6 +9,9 @@ module Redex
 				@order_id = cleaned_response[:NumPedido]
 				response_date_time = cleaned_response[:Data] + cleaned_response[:Hora] + '-03:00'
 				@created_at = DateTime.strptime(response_date_time, '%Y%m%d%H:%M:%S%z')
+				@credit_card_authorization_id = cleaned_response[:NumAutor]
+				@sequential_id = cleaned_response[:NumSqn].to_i
+				@transaction_id = cleaned_response[:Tid].to_i
 			end
 		end
 	end
--- spec/redex/response/transaction_response_spec.rb
+++ spec/redex/response/transaction_response_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 module Redex::Response
   RSpec.describe TransactionResponse do
-    let(:httpi_result) { {"Cet"=>[{"i:nil"=>"true"}], "CodRet"=>["00"], "Data"=>["20170725"], "Hora"=>["16:22:17"], "Juros"=>[{"i:nil"=>"true"}], "MsgAvs"=>[{"i:nil"=>"true"}], "Msgret"=>["Sucesso"], "NumAutor"=>["135985"], "NumPedido"=>["pedido123"], "NumSqn"=>["110087"], "RespAvs"=>[{"i:nil"=>"true"}], "Tid"=>["25458329533"], "ValParcelas"=>[{"i:nil"=>"true"}], "ValTotalJuros"=>[{"i:nil"=>"true"}]} }
+    let(:httpi_result) { {"Cet"=>[{"i:nil"=>"true"}], "CodRet"=>["00"], "Data"=>["20170725"], "Hora"=>["16:22:17"], "Juros"=>[{"i:nil"=>"true"}], "MsgAvs"=>[{"i:nil"=>"true"}], "Msgret"=>["Sucesso"], "NumAutor"=>["1BC3a4"], "NumPedido"=>["pedido123"], "NumSqn"=>["112233"], "RespAvs"=>[{"i:nil"=>"true"}], "Tid"=>["15462083544"], "ValParcelas"=>[{"i:nil"=>"true"}], "ValTotalJuros"=>[{"i:nil"=>"true"}]} }
     let(:transaction_response) { TransactionResponse.new(httpi_result) }
 
     it 'deve parsear o código de retorno' do
@@ -19,6 +19,18 @@ module Redex::Response
 
     it 'deve parsear data de retorno' do
       expect(transaction_response.created_at).to eq(DateTime.new(2017, 07, 25, 16, 22, 17,'-03:00'))
+    end
+
+    it 'deve parsear numero da autorização da operadora do cartao' do
+      expect(transaction_response.credit_card_authorization_id).to eq("1BC3a4")
+    end
+
+    it 'deve parsear número sequência da rede' do
+      expect(transaction_response.sequential_id).to eq(112233)
+    end
+
+    it 'deve parsear identicação da resposta' do
+      expect(transaction_response.transaction_id).to eq(15462083544)
     end
   end
 end
