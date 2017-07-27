eb38fa40a228425ece83bde5c1a1952ae1af352f
Diego Thomaz <diego@webgoal.com.br>
2017-07-27 17:05:54 -0300

    finaliza query response

--- lib/redex/response/query_response.rb
+++ lib/redex/response/query_response.rb
@@ -1,9 +1,85 @@
 module Redex
 	module Response
 		class QueryResponse < BaseResponse
-			attr_reader :canceled_at
+			attr_reader :canceled_at, :amount, :transaction_type, :installments, :order_id,
+				:card_number, :transaction_id, :card_expiration_month, :card_holder_name,
+				:invoice_note, :origin, :status, :transaction_created_at, :credit_card_authorization_id,
+				:code, :message
 			def initialize(httpi_response)
-				super(httpi_response)
+				super(httpi_response[:REGISTRO])
+			end
+
+			def amount
+				@amount ||= @raw[:TOTAL].to_f
+			end
+
+			def transaction_type
+				@transaction_type ||= @raw[:TRANSACAO].to_f
+			end
+
+			def installments
+				@installments ||= @raw[:PARCELAS].to_f
+			end
+
+			def order_id
+				@order_id ||= @raw[:NUMPEDIDO]
+			end
+
+			def card_number
+				@card_number ||= @raw[:NR_CARTAO]
+			end
+
+			def sequential_id
+				@sequential_id ||= @raw[:NUMSQN].to_i
+			end
+
+			def card_expiration_month
+				@card_expiration_month ||= @raw[:MES].to_i
+			end
+
+			def invoice_note
+				@invoice_note ||= @raw[:IDENTIFICACAOFATURA]
+			end
+
+			def card_holder_name
+				@card_holder_name ||= @raw[:NOM_PORTADOR]
+			end
+
+			def origin
+				@origin ||= @raw[:ORIGEM].to_i
+			end
+
+			def status
+				@status ||= @raw[:STATUS].to_i
+			end
+
+			def code
+				@code ||= @raw[:COD_RET].to_i
+			end
+
+			def message
+				@message ||= @raw[:MSG_RET]
+			end
+
+			def credit_card_authorization_id
+				@credit_card_authorization_id ||= @raw[:NUMAUTOR].to_i
+			end
+
+			def transaction_created_at
+				@transaction_created_at ||= datetime_sanitize(@raw[:DATA], @raw[:HORA])
+			end
+
+			def transaction_canceled_at
+				@transaction_canceled_at ||= datetime_sanitize(@raw[:DATA], "00:00:00")
+			end
+
+
+
+			def clear(httpi_result)
+				httpi_result.delete("AVS")
+				httpi_result = Hash[httpi_result.map { |k, v| [k.to_sym, v] }]
+				httpi_result.select! {|k,v| v != [{}]}
+				httpi_result.each { |k, v| httpi_result[k] = v.first }
 			end
 		end
 	end
--- spec/redex/response/query_response_spec.rb
+++ spec/redex/response/query_response_spec.rb
@@ -10,83 +10,75 @@ module Redex::Response
       "DATA_EXP_PRE_AUT"=>[{}], "FILIACAO_DSTR"=>["0"], "HORA"=>["15:00:01"],
       "IDENTIFICACAOFATURA"=>["Teste-Renovacao"], "MOEDA"=>["Real"],
       "MSG_RET"=>["Sucesso"], "NOM_PORTADOR"=>["Aluanetraiu"],
-      "NR_CARTAO"=>["544828XXXXXX0007"], "NUMAUTOR"=>["219947"],
-      "NUMPEDIDO"=>["34"], "NUMSQN"=>["90739"], "ORIGEM"=>["1"], "PARCELAS"=>["00"],
+      "NR_CARTAO"=>["544828XXXXXX0007"], "NUMAUTOR"=>["219947"], "MES"=>["3"],
+      "NUMPEDIDO"=>["a123123"], "NUMSQN"=>["90739"], "ORIGEM"=>["1"], "PARCELAS"=>["07"],
       "STATUS"=>["3"], "TAXA_EMBARQUE"=>["0.00"], "TID"=>["123123123123"],
       "TOTAL"=>["5009.00"], "TRANSACAO"=>["4"],
       "_x0033_DS"=>[{"CAVV"=>[{}], "ECI"=>[{}], "XID"=>[{}]}]}} }
 
     let(:transaction_response) { QueryResponse.new(httpi_result) }
 
-    xit 'deve parsear o valor total da compra' do
-      expect(transaction_response.total).to eq(5009)
+    it 'deve parsear o valor total da compra' do
+      expect(transaction_response.amount).to eq(5009)
     end
 
-    xit 'deve parsear o tipo de transacao' do
-      expect(transaction_response.transaction_type).to eq(0)
+    it 'deve parsear o tipo de transacao' do
+      expect(transaction_response.transaction_type).to eq(4)
     end
 
-    xit 'deve parsear o numeros de parcelas' do
-      expect(transaction_response.installments).to eq(0)
+    it 'deve parsear o numeros de parcelas' do
+      expect(transaction_response.installments).to eq(7)
     end
 
-    xit 'deve parsear o numeros pedido gerado pelo estabelecimento' do
-      expect(transaction_response.order_id).to eq(0)
+    it 'deve parsear o numeros pedido gerado pelo estabelecimento' do
+      expect(transaction_response.order_id).to eq("a123123")
     end
 
-    xit 'deve parsear o numero do cartao' do
-      expect(transaction_response.card_number).to eq(0)
+    it 'deve parsear o numero do cartao' do
+      expect(transaction_response.card_number).to eq("544828XXXXXX0007")
     end
 
-    xit 'deve parsear o numero sequencia da rede' do
-      expect(transaction_response.sequential_id).to eq(0)
+    it 'deve parsear o numero sequencia da rede' do
+      expect(transaction_response.sequential_id).to eq(90739)
     end
 
-    xit 'deve parsear o mês de validade do cartao' do
-      expect(transaction_response.card_expiration_month).to eq(0)
+    it 'deve parsear o mês de validade do cartao' do
+      expect(transaction_response.card_expiration_month).to eq(3)
     end
 
-    xit 'deve parsear o nome do portador do cartao' do
-      expect(transaction_response.card_holder_name).to eq(0)
+    it 'deve parsear o nome do portador do cartao' do
+      expect(transaction_response.card_holder_name).to eq("Aluanetraiu")
     end
 
-    xit 'deve parsear a mensagem que sera exibida ao lado do nome do estabelecimento' do
-      expect(transaction_response.invoice_note).to eq(0)
+    it 'deve parsear a mensagem que sera exibida ao lado do nome do estabelecimento' do
+      expect(transaction_response.invoice_note).to eq("Teste-Renovacao")
     end
 
-    xit 'deve parsear um numero que identifica a origem da transacao' do
-      expect(transaction_response.origin).to eq(0)
+    it 'deve parsear um numero que identifica a origem da transacao' do
+      expect(transaction_response.origin).to eq(1)
     end
 
-    xit 'deve parsear data da requisicao de transacao' do
-      expect(transaction_response.created_at).to eq(DateTime.new(2017, 07, 25, 16, 22, 17,'-03:00'))
+    it 'deve parsear o status' do
+      expect(transaction_response.status).to eq(3)
     end
 
-    xit 'deve parsear o status da data hora da requisicao' do
-      expect(transaction_response.status).to eq(0)
+    it 'deve parsear data da realizacao da transacao' do
+      expect(transaction_response.transaction_created_at).to eq(DateTime.new(2017, 07, 27, 15, 00, 01,'-03:00'))
     end
 
-    xit 'deve parsear data da realizacao da transacao' do
-      expect(transaction_response.transaction_created_at).to eq(DateTime.new(2017, 07, 25, 16, 22, 17,'-03:00'))
+    it 'deve parsear data de cancelamento' do
+      expect(transaction_response.transaction_canceled_at).to eq(DateTime.new(2017, 07, 27, 0, 0, 0, '-03:00'))
     end
 
-    xit 'deve parsear data de cancelamento' do
-      expect(transaction_response.transaction_canceled_at).to eq(DateTime.new(2017, 07, 25, 16, 22, 17,'-03:00'))
+    it 'deve parsear numero da autorização da operadora do cartao' do
+      expect(transaction_response.credit_card_authorization_id).to eq(219947)
     end
 
-    xit 'deve parsear identicação da resposta' do
-      expect(transaction_response.transaction_id).to eq(15462083544)
-    end
-
-    xit 'deve parsear numero da autorização da operadora do cartao' do
-      expect(transaction_response.credit_card_authorization_id).to eq(123123)
-    end
-
-    xit 'deve parsear o código de retorno' do
+    it 'deve parsear o código de retorno' do
       expect(transaction_response.code).to eq(0)
     end
 
-    xit 'deve parsear mensagem de retorno' do
+    it 'deve parsear mensagem de retorno' do
       expect(transaction_response.message).to eq("Sucesso")
     end
   end
